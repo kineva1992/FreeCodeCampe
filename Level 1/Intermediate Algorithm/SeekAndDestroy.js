434c08b72c19b70b576daaa71253c662fac4d251
kineva1992 <42785264+kineva1992@users.noreply.github.com>
2021-08-18 13:52:41 +0300

    Update SeekAndDestroy.js

--- Level 1/Intermediate Algorithm/SeekAndDestroy.js
+++ Level 1/Intermediate Algorithm/SeekAndDestroy.js
@@ -1,1 +1,9 @@
+function destroyer(arr) {
+  const destroyerArr = Object.values(arguments).slice(1);
 
+  return arr.filter(x => !destroyerArr.find(y => y === x));
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
